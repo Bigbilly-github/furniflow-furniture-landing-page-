53eb32ea3685c90c7f7daa056f29aac52413f748
Bigbilly-github <abiolatwitter97@gmail.com>
2025-07-10 09:37:25 +0100

    added fonts "inter"and "montserat"

---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+     <header className='flex w-full justify-center items-center h-[100px] bg-[#35736E]'>
+        <p className=' font-bold'>
+            love
+        </p>
+
+     </header>
+    </>
+    
+  )
+}
+
+export default Header
